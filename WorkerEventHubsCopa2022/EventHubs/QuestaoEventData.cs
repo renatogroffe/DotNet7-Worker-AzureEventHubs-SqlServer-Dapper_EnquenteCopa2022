b5e93ba2c7b7fe27099cb5d6e66f9befffd2e49c
Renato Groffe <renato.groffe@yahoo.com.br>
2022-12-07 17:54:42 -0300

    .NET 7 + Worker Service + Azure Event Hubs +SQL Server + Dapper.Contrib + Application Insights (via Connection String) + Dockerfile + Pesquisa Interesses (Votos)
    .NET 7 + Worker Service + Azure Event Hubs +SQL Server + Dapper.Contrib + Application Insights (via Connection String) + Dockerfile + Pesquisa Interesses (Votos)

---
+++ WorkerEventHubsCopa2022/EventHubs/QuestaoEventData.cs
@@ -0,0 +1,9 @@
+namespace WorkerEventHubsCopa2022.EventHubs;
+
+public class QuestaoEventData
+{
+    public string? IdVoto { get; set; }
+    public string? Horario { get; set; }
+    public string? Producer { get; set; }
+    public string? Pais { get; set; }
+}
